901edb6198ee2ca42601e773c6b5328f4f597018
yahya_lazar <118053432+Yahyalazar@users.noreply.github.com>
2024-10-07 01:10:13 +0000

    any

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -75,7 +75,7 @@ const Dashboard = () => {
         }
     };
 
-    const formatCurrency = (value) => {
+    const formatCurrency = (value:any) => {
         return value?.toLocaleString('en-US', {
             style: 'currency',
             currency: 'USD',
