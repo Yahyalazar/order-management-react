6b7f9a452252fe5143396cf28a037f03bedef7d1
yahya_lazar <118053432+Yahyalazar@users.noreply.github.com>
2024-10-10 17:08:26 +0000

    las t

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -162,7 +162,7 @@ const Dashboard: React.FC = () => {
                     <div className="card mb-0">
                         <div className="flex justify-content-between mb-3">
                             <div>
-                                <span className="block text-500 font-medium mb-3">الرصيد المستلم</span>
+                                <span className="block text-500 font-medium mb-3">الرصيد تم التسليم</span>
                                 <div className="text-900 font-medium text-xl">${usersCount}</div>
                             </div>
                             <div className="flex align-items-center justify-content-center bg-purple-100 border-round" style={{ width: '2.5rem', height: '2.5rem' }}>
